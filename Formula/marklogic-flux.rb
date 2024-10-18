8ce31a417ff93103e44a7720b621f6d7a1f04d5e
Chris Box <chris.box@marklogic.com>
2024-10-18 08:04:29 -0400

    Upgrade Flux to version 1.1.2

--- Formula/marklogic-flux.rb
+++ Formula/marklogic-flux.rb
@@ -1,8 +1,8 @@
 class MarklogicFlux < Formula
     desc "Application for all of your data movement use cases with MarkLogic"
     homepage "https://marklogic.github.io/flux/"
-    url "https://github.com/marklogic/flux/releases/download/1.1.1/marklogic-flux-1.1.1.zip"
-    sha256 "11ee6fb25d8e692f9e01f494958247ad36a4498f3923be0f82f2b380e7e4cf7f"
+    url "https://github.com/marklogic/flux/releases/download/1.1.2/marklogic-flux-1.1.2.zip"
+    sha256 "dd2c891e64ef0f849b9ed63ce5f84c499bfb59c0e9322fc846ada4ffaa1f1ba8"
     license "Apache-2.0"
   
     depends_on "openjdk@11" => :optional
